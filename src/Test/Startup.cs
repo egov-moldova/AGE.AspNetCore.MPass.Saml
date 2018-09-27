71fda34c030edd186a60efb6d17a7b6ddc3d4b9a
Contantin Grigoras <constantin.grigoras@egov.md>
2018-09-27 17:09:40 +0300

    Gitignore change

--- src/Test/Startup.cs
+++ src/Test/Startup.cs
@@ -40,9 +40,14 @@ namespace Test
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
                 app.UseExceptionHandler("/Error");
-            
+            }
             app.UseStatusCodePages();
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
